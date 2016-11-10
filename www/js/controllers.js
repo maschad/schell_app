95b60f2767afd2b1541eb1326f1f692dcc82abba
maschad <chad.nehemiah94@gmail.com>
2016-11-09 22:05:35 -0500

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,66 @@
+angular.module('app.controllers', [])
+  
+.controller('start_screenCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('productsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('videoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('countryselectCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('detailPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('bookmarkCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,81 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.start_screen', {
+    url: '/start_screen',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/start_screen.html',
+        controller: 'start_screenCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.products', {
+    url: '/product_areas',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/products.html',
+        controller: 'productsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('video', {
+    url: '/video',
+    templateUrl: 'templates/video.html',
+    controller: 'videoCtrl'
+  })
+
+  .state('settings', {
+    url: '/settings',
+    templateUrl: 'templates/settings.html',
+    controller: 'settingsCtrl'
+  })
+
+  .state('countryselect', {
+    url: '/country_select',
+    templateUrl: 'templates/countryselect.html',
+    controller: 'countryselectCtrl'
+  })
+
+  .state('tabsController.detailPage', {
+    url: '/detail_page',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/detailPage.html',
+        controller: 'detailPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.bookmark', {
+    url: '/bookmark_list_detail',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/bookmark.html',
+        controller: 'bookmarkCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('')
+
+  
+
+});
