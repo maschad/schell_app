28d063bf06ba1291eed3df43a52d6b0a23945a3a
maschad <chad.nehemiah94@gmail.com>
2016-11-17 23:22:06 -0500

    updated detail page, need to figure out how to display images

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -112,7 +112,13 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-
+  $scope.products = [
+    {
+      name: 'TECHNISCHE ZEICHNUNG',
+      description: 'img',
+      show: false
+    }
+  ]
 
 }])
 
