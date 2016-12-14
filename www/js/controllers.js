ea14373d0a1cf82bb01507ce98eafe4a52a411ac
maschad <chad.nehemiah94@gmail.com>
2016-12-14 11:55:06 -0500

    removed irrelevant menuctrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -137,13 +137,6 @@ function ($scope, $state, $ionicPopover,$rootScope,$ionicSideMenuDelegate) {
 
   }])
 
-.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
 
 .controller('videoCtrl', ['$scope', '$sce', '$ionicLoading','DataService',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
