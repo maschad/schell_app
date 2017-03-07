667146869029090e5762e5b4d903acd8a6b705b8
ANich <alexj.nich@hotmail.com>
2017-03-07 14:39:43 -0800

    Fix history when viewing bookmarked products
    (and searched products)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -786,7 +786,6 @@ function ($scope, $state, $ionicSideMenuDelegate,localStorageService) {
         }
       };
 
-
       //Side Menu
       $ionicSideMenuDelegate.canDragContent(false);
 
@@ -801,11 +800,12 @@ function ($scope, $state, $ionicSideMenuDelegate,localStorageService) {
 
       //History function
       var goback = $scope.$on('go-back', function () {
-
         appDataService.removeNavigatedCategory();
         var pop = appDataService.getCurrentCategoryIds();
-        if ($rootScope.navigated_categories.includes('MERKZETTEL') || $rootScope.navigated_categories.includes('SUCHE')) {
-          $ionicHistory.goBack();
+        if ($rootScope.navigated_categories.includes('MERKZETTEL')) {
+          $state.go('bookmark');
+        } else if ($rootScope.navigated_categories.includes('SUCHE')) {
+          $state.go('searchPage');
         } else {
           $state.go('product_overview');
         }
@@ -1838,9 +1838,8 @@ function ($scope, $state, $ionicSideMenuDelegate,localStorageService) {
         $state.go('start-screen');
       });
 
-      //Add Home as default
+      appDataService.clearNavigatedCategories();
       appDataService.addNavigatedCategory('PRODUKTE');
-      //Add search to navigated categories.
       appDataService.addNavigatedCategory('MERKZETTEL');
 
       //Check to see if any bookmark products should be removed.
@@ -1848,7 +1847,6 @@ function ($scope, $state, $ionicSideMenuDelegate,localStorageService) {
       if ($rootScope.internet) {
         FirebaseService.checkBookmark(localStorageService.getBookmarkedProducts());
       }
-
 
       //Download bookmarks
       $scope.bookmarks = localStorageService.getBookmarkedProducts();
